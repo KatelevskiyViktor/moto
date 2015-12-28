3928d35f523dee51d516e1d0261717e92cc3f98f
&& <empirikk@ya.ru>
2015-12-29 02:18:08 +0300

    Добавление файлов для moto

---
+++ js/myScripts.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    function isNumeric(n) {
+        return !isNaN(parseFloat(n)) && isFinite(n);
+    }
+function hideON(idEl, time){
+    if(isNumeric(time) && time > 500 && time < 10000)idEl.fadeOut(time);
+    else alert("Вы ввели не число, либо оно вне диапазона!");
+
+}
+    hideON($('img[src*=logo]'), 300);
+
+});//Конец ready
+
+//$('#moto_models').html();-возвращает либо содержимое элемента либо меняет его
+//$('#div_for_img').width('600').height(); - возвращают либо размер элемента либо меняют его
+//$('a[href*=PDF]').hide().show(2000);$('img[src*=logo]').fadeOut(4000).fadeIn(4000);-исчезновение и появление элиментов на странице
+//$('#moto_table tr:odd');
+//$('#moto_table tr:even');
+//$('img:not(#div_for_img img)');
+//$('div:has(fieldset)');
+//$('p:contains(мотоцикл)');
+//$('#div_for_img img:first');
+//$('#div_for_img img:last');
+//$('div:hidden');
+//$('div:visibility');
+//$('img[src*=moto]');
+//$('img[src$=.jpg]');
+//$('a[href^=http]');
+//$('img[width=200]');
+//$('#div_for_img > img');
+//$('#main_h1 + p');
+//$('#div_for_img img');
+//$('.maindiv');
+//$('#main_h1');
+//$('p');
